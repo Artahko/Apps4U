8cab1f898b48493f15f129c94a256704e8c8fdd8
Artahko <artem4iky8@gmail.com>
2026-02-16 18:17:34 +0200

    Update urls.py

--- Apps4U/django/mysite/urls.py
+++ Apps4U/django/mysite/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.course_list, name='course_list'),
-    path('<slug:course_slug>/', views.course_detail, name='course_detail'),
-    path('<slug:course_slug>/faq/', views.course_faq, name='course_faq'),
-    path("<slug:course_slug>/activities/", views.course_activities, name="course_activities"),
+    path('admin/', admin.site.register),
+    path('', include('courses.urls')),
 ]
