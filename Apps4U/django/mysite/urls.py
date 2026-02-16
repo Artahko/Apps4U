9d3f58e25623239f9f7f762adb180aa01e8160a1
Tswugh <137005971+Tswugh@users.noreply.github.com>
2026-02-16 17:53:25 +0200

    Add accounts URL pattern for allauth
    Added URL pattern for account management using allauth.

--- Apps4U/django/mysite/urls.py
+++ Apps4U/django/mysite/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<slug:course_slug>/', views.course_detail, name='course_detail'),
     path('<slug:course_slug>/faq/', views.course_faq, name='course_faq'),
     path("<slug:course_slug>/activities/", views.course_activities, name="course_activities"),
+    path("accounts/", include("allauth.urls"))
 ]
+
