ef5659aec8229d63773c8f2ebf010ea1e124b239
Marichka-chicheri <maryfedorenko08@gmail.com>
2026-02-17 15:46:07 +0200

    added comments for activities

--- Apps4U/django/courses/urls.py
+++ Apps4U/django/courses/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .views import AddCommentView
 
 urlpatterns = [
     path('', views.course_list, name='course_list'),
     path('<slug:course_slug>/faq/', views.course_faq, name='course_faq'),
     path("<slug:course_slug>/activities/<int:activity_id>", views.activity_detail, name="activity_detail"),
+    path("<slug:course_slug>/activities/<int:activity_id>/comment", AddCommentView.as_view(), name="add_comment"),
     path("<slug:course_slug>/activities/", views.course_activities, name="course_activities"),
     path('<slug:course_slug>/', views.course_detail, name='course_detail'),
 ]
