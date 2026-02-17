6c902a95b41e79fa2eb1e408093f8300a2303bd7
Marichka-chicheri <maryfedorenko08@gmail.com>
2026-02-17 11:55:04 +0200

    sho tse

--- Apps4U/django/courses/admin.py
+++ Apps4U/django/courses/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Course, Activity, FAQ
+from .models import Course, Activity, FAQ, Comment
 
 admin.site.register(Course)
 admin.site.register(Activity)
 admin.site.register(FAQ)
+admin.site.register(Comment)
