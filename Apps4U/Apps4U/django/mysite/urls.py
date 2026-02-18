8c42665b5c998dfd949936b2cec6abb047ccfc6c
Marichka-chicheri <maryfedorenko08@gmail.com>
2026-02-18 11:18:05 +0200

    changes

--- Apps4U/Apps4U/django/mysite/urls.py
+++ Apps4U/Apps4U/django/mysite/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,4 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('сourses/', include('courses.urls')),
+    path('profile/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Apps4U/Apps4U/django/users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    else:
+        form = ProfileUpdateForm(instance=request.user.profile)
+
+    return render(request, 'users/edit_profile.html', {'form': form})
+
+def profile(request):
+    return render(request, "users/profile.html")
