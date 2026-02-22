7ecee1f79a13ac4c8ed3466c3c631a7c7cb71216
Marichka-chicheri <maryfedorenko08@gmail.com>
2026-02-22 15:07:08 +0200

    sw branches

--- Apps4U/Apps4U/django/users/forms.py
+++ Apps4U/Apps4U/django/users/forms.py
@@ -4,4 +4,4 @@ from .models import Profile
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['about', 'icon', 'major', 'year_of_study']
+        fields = ['icon', 'about']
