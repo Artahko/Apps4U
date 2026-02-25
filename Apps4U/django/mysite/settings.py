924beae876e1bbf073d5198d7316f70077f2a0f2
Artahko <artem4iky8@gmail.com>
2026-02-25 16:30:23 +0200

    added normal 404 error and base.html+footter

--- Apps4U/django/mysite/settings.py
+++ Apps4U/django/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)n09z2(i1mn7_vwtmw(r3s6zc9c6m8ay(d+u%$(#pxupcsn95y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
